ed45d3914922b1ed4443291660cfe9a00fcd0ca7
Youncle <carstocea_vladimir@yahoo.com>
2018-01-23 15:57:30 +0200

    3rd project

---
+++ get_next_line.h
@@ -0,0 +1,18 @@
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# define BUFF_SIZE 99
+
+# include "libft/libft.h"
+# include <fcntl.h>
+
+int						get_next_line(int const fd, char **line);
+
+typedef struct			s_list_gnl
+{
+	int					fd;
+	char				*temp;
+	struct s_list_gnl	*next;
+}						t_list_gnl;
+
+#endif
